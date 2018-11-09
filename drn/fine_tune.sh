41a8319e81d2e68f2478dc00d119a4b7ab833eac
Kelvin <kelvinhu9988@gmail.com>
2018-11-09 11:14:38 -0500

    Added fine tuning script

---
+++ drn/fine_tune.sh
@@ -0,0 +1,28 @@
+
+#!/bin/bash
+
+SBATCH -J fine_tune_bdd        # Job name
+SBATCH -o fine_tune_bdd.o%j    # Name of stdout output file (%j expands to jobId)
+SBATCH -e fine_tune_bdd.o%j    # Name of stderr output file
+SBATCH -N 1                    # Total number of CPU nodes requested
+SBATCH -n 1                    # Total number of CPU cores requrested
+SBATCH --mem=5G                # CPU Memory pool for all cores
+SBATCH -t 48:00:00             # Run time (hh:mm:ss)
+SBATCH --partition=default_gpu --gres=gpu:2    # Which queue to run on, and what resources to use
+                                               # --partition=<queue> - Use the `<queue>` queue
+                                               # --gres=gpu:1 - Use 1 GPU of any type
+                                               # --gres=gpu:1080ti:1 - Use 1 GTX 1080TI GPU
+
+# OPTIONAL: Uncomment this if you're using an anaconda environment
+source /home/sh2442/anaconda3/etc/profile.d/conda.sh
+conda activate pytorch
+
+
+# OPTIONAL: Uncomment this if you need to copy a dataset over to scratch
+#           This checks to see if the dataset already exists
+if [ ! -d /scratch/datasets/bdd ]; then
+   cp -r /home/sh2442/bdd /scratch/datasets/bdd
+fi
+
+python3 segment.py train -d datasets/bdd/seg -c 19 -s 896 \
+    --arch drn_d_22 --batch-size 32 --epochs 250 --lr 0.01 --momentum 0.9 --step 100 
--- drn/run_train.sh
+++
@@ -1,6 +0,0 @@
-# Compute mean and std
-python compute_mean_std.py --data-dir bdd/seg
-
-# Fine tune the pretrained Cityscapes model on BDD
-python3 segment.py train -d datasets/bdd/seg -c 19 -s 896 \
-    --arch drn_d_22 --batch-size 32 --epochs 250 --lr 0.01 --momentum 0.9 --step 100
