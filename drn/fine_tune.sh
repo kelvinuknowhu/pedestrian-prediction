600a1ed3383160312a2227cf9d0e0cde523bb341
Kelvin <kelvinhu9988@gmail.com>
2018-11-09 12:02:38 -0500

    Updated fine tune

--- drn/fine_tune.sh
+++ drn/fine_tune.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-SBATCH -J fine_tune_bdd        # Job name
-SBATCH -o fine_tune_bdd.o%j    # Name of stdout output file (%j expands to jobId)
-SBATCH -e fine_tune_bdd.o%j    # Name of stderr output file
-SBATCH -N 1                    # Total number of CPU nodes requested
-SBATCH -n 1                    # Total number of CPU cores requrested
-SBATCH --mem=5G                # CPU Memory pool for all cores
-SBATCH -t 48:00:00             # Run time (hh:mm:ss)
-SBATCH --partition=default_gpu --gres=gpu:2    # Which queue to run on, and what resources to use
+#SBATCH -J fine_tune_bdd        # Job name
+#SBATCH -o fine_tune_bdd.o%j    # Name of stdout output file (%j expands to jobId)
+#SBATCH -e fine_tune_bdd.o%j    # Name of stderr output file
+#SBATCH -N 1                    # Total number of CPU nodes requested
+#SBATCH -n 1                    # Total number of CPU cores requrested
+#SBATCH --mem=5G                # CPU Memory pool for all cores
+#SBATCH -t 48:00:00             # Run time (hh:mm:ss)
+#SBATCH --partition=default_gpu --gres=gpu:2    # Which queue to run on, and what resources to use
                                                # --partition=<queue> - Use the `<queue>` queue
                                                # --gres=gpu:1 - Use 1 GPU of any type
                                                # --gres=gpu:1080ti:1 - Use 1 GTX 1080TI GPU
