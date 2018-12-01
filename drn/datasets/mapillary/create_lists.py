6642ca4eefae1b5e896b40e49abb0332135b620b
Kelvin <kelvinhu9988@gmail.com>
2018-11-30 21:20:20 -0500

    Update create lists

--- drn/datasets/mapillary/create_lists.py
+++ drn/datasets/mapillary/create_lists.py
@@ -5,25 +5,21 @@ val_images_dir = 'validation/images'
 val_labels_dir = 'validation/train_ids'
 test_images_dir = 'testing/images'
 
-f = open('train_images.txt', 'w')
-for fn in sorted(os.listdir(train_images_dir)):
-    f.write(train_images_dir + '/' + fn + '\n')
-f.close()
-
-f = open('train_labels.txt', 'w')
+f_labels = open('train_labels.txt', 'w')
+f_images = open('train_images.txt', 'w')
 for fn in sorted(os.listdir(train_labels_dir)):
-    f.write(train_labels_dir + '/' + fn + '\n')
-f.close()
+    f_labels.write(train_labels_dir + '/' + fn + '\n')
+    f_images.write(train_images_dir + '/' + fn.split('.')[0] + '.jpg' + '\n')
+f_labels.close()
+f_images.close()
 
-f = open('val_images.txt', 'w')
-for fn in sorted(os.listdir(val_images_dir)):
-    f.write(val_images_dir + '/' + fn + '\n')
-f.close()
-
-f = open('val_labels.txt', 'w')
+f_labels = open('val_labels.txt', 'w')
+f_images = open('val_images.txt', 'w')
 for fn in sorted(os.listdir(val_labels_dir)):
-    f.write(val_labels_dir + '/' + fn + '\n')
-f.close()
+    f_labels.write(val_labels_dir + '/' + fn + '\n')
+    f_images.write(val_images_dir + '/' + fn.split('.')[0] + '.jpg' + '\n')
+f_labels.close()
+f_images.close()
 
 f = open('test_images.txt', 'w')
 for fn in sorted(os.listdir(test_images_dir)):
